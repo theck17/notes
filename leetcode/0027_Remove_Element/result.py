90629a32c8edd889bcc2f1c47c40d5337a1a2fac
theck17 <theck17@163.com>
2021-01-15 21:06:44 +0800

    0027

--- leetcode/0027_Remove_Element/result.py
+++ leetcode/0027_Remove_Element/result.py
@@ -1,4 +1,9 @@
-
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        for i in range(nums.count(val)):
+            valIndex = nums.index(val)
+            del nums[valIndex]
+        return len(nums)
 
 if __name__ == '__main__':
     pass
