036b2b3104e6ab4d10cf6dffa2bdb585d139d137
theck17 <theck17@163.com>
2021-01-21 21:35:56 +0800

    66

--- leetcode/0066_Plus_One/result.py
+++ leetcode/0066_Plus_One/result.py
@@ -4,5 +4,19 @@
 # DateTime:2021-01-21 21:30:34
 # Description:
 
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        token = 1
+        for i in range(len(digits) - 1, -1, -1):
+            sum = digits[i] + token
+            digits[i] = sum % 10
+            token = sum // 10
+        if token > 0:
+            return [token] + digits
+        else:
+            return digits
+
+
 if __name__ == "__main__":
     pass
