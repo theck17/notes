22ee26f160bebd79e5db0c05368af690ed82c0e2
theck17 <theck17@163.com>
2021-01-21 21:42:03 +0800

    66

---
+++ leetcode/0066_Plus_One/result.c
@@ -0,0 +1,16 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.c
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-01-21 21:41:50
+ *   Description   ：
+ */
+#include <ctype.h>  //字符函数
+#include <math.h>   //数学函数
+#include <stdio.h>  //输入输出函数
+#include <stdlib.h> //动态分配函数和随机函数
+#include <string.h> //字符串函数
+
+int main() { return 0; }
