5dc30adee508764bcdca566d15913e59ab0c8241
theck17 <theck17@163.com>
2021-01-28 22:15:40 +0800

    119

--- leetcode/0119_Pascals_Triangle_2/result.cpp
+++ leetcode/0119_Pascals_Triangle_2/result.cpp
@@ -15,6 +15,19 @@
 #include <istream>         //基本输入流
 #include <iterator>        //STL迭代器
 #include <ostream>         //基本输出流
+#include <queue>           //STL 队列容器
+#include <set>             //STL 集合容器
+#include <sstream>         //基于字符串的流
+#include <stack>           //STL 堆栈容器
+#include <stdexcept>       //标准异常类
+#include <streambuf>       //底层输入／输出支持
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+
 class Solution {
     long long computeProduct(int a, int b, int c) {
         long long product=1;
