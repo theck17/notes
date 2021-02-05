3116c39ff28b363247e8b9e07ee78ff1d88b6b04
theck17 <theck17@163.com>
2021-02-05 20:33:50 +0800

    122

--- leetcode/0122_Best_Time_to_Buy_and_Sell_Stock_2/result.py
+++ leetcode/0122_Best_Time_to_Buy_and_Sell_Stock_2/result.py
@@ -2,26 +2,25 @@
 # Author: C.K
 # Email: theck17@163.com
 # DateTime:2021-02-04 21:51:27
-# Description: 
+# Description:
+
+import os, sys, re
+
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        cash, hold = 0, -prices[0]        
+        cash, hold = 0, -prices[0]
         for i in range(len(prices)):
             cash = max(cash, hold + prices[i])
             hold = max(hold, cash - prices[i])
         return cash
-        
-		
+
     def maxProfit(self, prices: List[int]) -> int:
-        ans, n = 0, len(prices)       
+        ans, n = 0, len(prices)
         for i in range(1, n):
             ans += max(0, prices[i] - prices[i - 1])
         return ans
 
 
-
-
 if __name__ == "__main__":
-   pass
-
+    pass
