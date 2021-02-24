e5e4bbee70b5917733c1a66b08c417790036420c
theck17 <theck17@163.com>
2021-02-24 21:21:38 +0800

    27

--- leetcode/0027_Remove_Element/result.py
+++ leetcode/0027_Remove_Element/result.py
@@ -1,9 +1,17 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-02-24 21:21:24
+# Description:
+
+
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         for i in range(nums.count(val)):
             valIndex = nums.index(val)
             del nums[valIndex]
         return len(nums)
+
 
 if __name__ == '__main__':
     pass
