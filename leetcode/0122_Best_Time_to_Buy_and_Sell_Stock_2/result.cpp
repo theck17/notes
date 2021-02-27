9e91d7ef8d17600d23bf6e7396b220370a66d7b8
theck17 <theck17@163.com>
2021-02-27 18:51:02 +0800

    122

--- leetcode/0122_Best_Time_to_Buy_and_Sell_Stock_2/result.cpp
+++ leetcode/0122_Best_Time_to_Buy_and_Sell_Stock_2/result.cpp
@@ -7,6 +7,8 @@
  *   DateTime      ：2021-02-04 21:50:26
  *   Description   ：
  */
+#include <stdexcept>       //标准异常类
+#include <streambuf>       //底层输入／输出支持
 #include <string>          //字符串类
 #include <vector>          //STL 动态数组容器
 #include <valarray>        //对包含值的数组的操作
