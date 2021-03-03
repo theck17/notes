f9fda201cd23f4a92f7965fe3225729bcc48c3a7
theck17 <theck17@163.com>
2021-03-03 19:47:18 +0800

    414

---
+++ leetcode/0414_Third_Maximum_Number/result.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-03-03 19:47:00
+# Description:
+
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = set(nums)
+        if len(nums) <= 2:
+            return max(nums)
+        elif len(nums) > 2:
+            counter = 0
+            while counter < 2:
+                nums.remove(max(nums))
+                counter += 1
+            return max(nums)
+
+
+if __name__ == "__main__":
+    pass
