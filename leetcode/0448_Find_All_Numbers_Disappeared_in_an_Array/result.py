4683d308114b6e6dfd2fe853e50ad3bc4bcad3a6
theck17 <theck17@163.com>
2021-03-08 19:43:47 +0800

    448

---
+++ leetcode/0448_Find_All_Numbers_Disappeared_in_an_Array/result.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-03-08 19:43:29
+# Description: 
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        arr = set(range(1, len(nums) + 1))
+        for num in nums:
+          if num in arr:
+            arr.remove(num)
+        return list(arr)
+
+
+
+
+if __name__ == "__main__":
+   pass
+
