07e3c6747db1ceaabd03d6eb617343452387b945
theck17 <theck17@163.com>
2021-03-12 21:27:12 +0800

    448

--- leetcode/0448_Find_All_Numbers_Disappeared_in_an_Array/result.py
+++ leetcode/0448_Find_All_Numbers_Disappeared_in_an_Array/result.py
@@ -2,21 +2,17 @@
 # Author: C.K
 # Email: theck17@163.com
 # DateTime:2021-03-08 19:43:29
-# Description: 
+# Description:
 
-import os, sys
 
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         arr = set(range(1, len(nums) + 1))
         for num in nums:
-          if num in arr:
-            arr.remove(num)
+            if num in arr:
+                arr.remove(num)
         return list(arr)
 
 
-
-
 if __name__ == "__main__":
-   pass
-
+    pass
