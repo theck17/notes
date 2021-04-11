a12f95c51c18fa9d05b8807b7065f29489e2b543
theck17 <theck17@163.com>
2021-04-11 11:21:36 +0800

    42

---
+++ leetcode/0042_Trapping_Rain_Water/result.py
@@ -0,0 +1,36 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-04-11 11:21:18
+# Description:
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+
+        left_max = 0
+        right_max = 0
+
+        ans = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] > left_max:
+                    left_max = height[left]
+                else:
+                    ans += left_max - height[left]
+                left += 1
+            else:
+                if height[right] > right_max:
+                    right_max = height[right]
+                else:
+                    ans += right_max - height[right]
+                right -= 1
+
+        return ans
+
+
+if __name__ == "__main__":
+    pass
