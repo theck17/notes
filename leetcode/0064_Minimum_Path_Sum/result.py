3c51bbaf8acde2677367c9ea9ad83462be7db69c
theck17 <theck17@163.com>
2021-04-19 18:25:01 +0800

    64

---
+++ leetcode/0064_Minimum_Path_Sum/result.py
@@ -0,0 +1,31 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-04-19 18:24:46
+# Description:
+
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+
+        dp[0][0] = grid[0][0]
+
+        for i in range(1, m):
+            dp[i][0] = dp[i - 1][0] + grid[i][0]
+
+        for j in range(1, n):
+            dp[0][j] = dp[0][j - 1] + grid[0][j]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1])
+
+        return dp[-1][-1]
+
+
+if __name__ == "__main__":
+    pass
