0617889400175845e64d37af898cff96db58de06
theck17 <theck17@163.com>
2021-04-23 19:02:30 +0800

    74

---
+++ leetcode/0074_Search_a_2D_Matrix/result.py
@@ -0,0 +1,28 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-04-23 19:02:11
+# Description:
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n = len(matrix)
+        for data in matrix:
+            start = 0
+            end = len(data) - 1
+            if data[start] <= target <= data[end]:
+                while start <= end:
+                    mid = (start + end) // 2
+                    if data[mid] == target:
+                        return True
+                    elif data[mid] < target:
+                        start = mid + 1
+                    else:
+                        end = mid - 1
+                break
+        return False
+
+
+if __name__ == "__main__":
+    pass
