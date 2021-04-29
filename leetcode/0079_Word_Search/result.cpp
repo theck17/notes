e05f3f43c1ed55ac1402808715f98d3cb4192239
theck17 <theck17@163.com>
2021-04-29 21:20:37 +0800

    79

---
+++ leetcode/0079_Word_Search/result.cpp
@@ -0,0 +1,52 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-04-29 21:20:08
+ *   Description   ：
+ */
+#include <algorithm>        //STL 通用算法
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+
+class Solution {
+private:
+    bool dfs(vector<vector<char>>& board, int row, int col, const string &word, int start, int M, int N, int sLen)
+    {
+        char curC;
+        bool res = false;
+        if( (curC = board[row][col]) != word[start]) return false;
+        if(start==sLen-1) return true;
+        board[row][col] = '*';
+        if(row>0) res = dfs(board, row-1, col, word, start+1, M, N, sLen);
+        if(!res && row < M-1) res = dfs(board, row+1, col, word, start+1, M, N, sLen);
+        if(!res && col > 0)   res = dfs(board, row, col-1, word, start+1, M, N, sLen);
+        if(!res && col < N-1) res = dfs(board,  row, col+1, word, start+1, M, N, sLen);
+        board[row][col] = curC;
+        return res;
+    }
+    
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int M,N,i,j,sLen = word.size();
+        if( (M=board.size()) && (N=board[0].size()) && sLen)
+        {
+            for(i=0; i<M; ++i)
+                for(j=0; j<N; ++j)
+                    if(dfs(board, i, j, wod, 0, M, N, sLen)) return true;
+        }
+        return false;
+    }
+};r
+
+
+
+int main(){
+    return 0;
+}
