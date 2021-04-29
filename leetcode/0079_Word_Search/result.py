480dac6f5b020c88399006541a1935d93de5c989
theck17 <theck17@163.com>
2021-04-29 18:47:08 +0800

    79

---
+++ leetcode/0079_Word_Search/result.py
@@ -0,0 +1,35 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-04-29 18:46:53
+# Description:
+
+
+class Solution():
+    def exist(self, board, word):
+        if not board:
+            return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.dfs(board, i, j, word):
+                    return True
+        return False
+
+    # check whether can find word, start at (i,j) position
+    def dfs(self, board, i, j, word):
+        if len(word) == 0: # all the characters are checked
+            return True
+        if i < 0 or i >= len(board) or j < 0 or j >= len(
+                board[0]) or word[0] != board[i][j]:
+            return False
+        tmp = board[i][j] # first character is found, check the remaining part
+        board[i][j] = "#" # avoid visit agian
+        # check whether can find "word" along one direction
+        res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) \
+        or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
+        board[i][j] = tmp
+        return res
+
+
+if __name__ == "__main__":
+    pass
