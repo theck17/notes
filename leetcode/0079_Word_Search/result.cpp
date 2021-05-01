003af385b28ce7c0c700c7f0d2c1e00db283145a
theck17 <theck17@163.com>
2021-05-02 00:29:12 +0800

    79

--- leetcode/0079_Word_Search/result.cpp
+++ leetcode/0079_Word_Search/result.cpp
@@ -8,7 +8,9 @@
  *   Description   ：
  */
 #include <algorithm>        //STL 通用算法
+#include <string>          //字符串类
 #include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
 using namespace std;
 
 
