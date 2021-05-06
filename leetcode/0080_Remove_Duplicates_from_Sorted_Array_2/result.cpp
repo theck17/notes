e707437418e9241b094006d98cfcd2c2c1d61971
theck17 <theck17@163.com>
2021-05-06 15:24:13 +0800

    80

---
+++ leetcode/0080_Remove_Duplicates_from_Sorted_Array_2/result.cpp
@@ -0,0 +1,33 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-05-06 15:23:53
+ *   Description   ：
+ */
+#include <algorithm>        //STL 通用算法
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for (int n : nums)
+            if (i < 2 || n > nums[i-2])
+                nums[i++] = n;
+        return i;
+    }
+};
+
+
+
+
+int main(){
+    return 0;
+}
