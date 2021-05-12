d88b38ffc12fe23efc21325236a6db44a4b3daf8
theck17 <theck17@163.com>
2021-05-12 19:33:31 +0800

    85

---
+++ leetcode/0085_Maximal_Rectangle/result.py
@@ -0,0 +1,29 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-05-12 19:33:10
+# Description:
+
+
+class Solution:
+    def maximalRectangle(self, matrix):
+        if not matrix or not matrix[0]:
+            return 0
+        n = len(matrix[0])
+        height = [0] * (n + 1)
+        ans = 0
+        for row in matrix:
+            for i in range(n):
+                height[i] = height[i] + 1 if row[i] == '1' else 0
+            stack = [-1]
+            for i in range(n + 1):
+                while height[i] < height[stack[-1]]:
+                    h = height[stack.pop()]
+                    w = i - 1 - stack[-1]
+                    ans = max(ans, h * w)
+                stack.append(i)
+        return ans
+
+
+if __name__ == "__main__":
+    pass
