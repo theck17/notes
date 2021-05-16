82caed3c6ad110feac6aef631ad23e77b8664f86
theck17 <theck17@163.com>
2021-05-17 00:34:41 +0800

    105

---
+++ leetcode/0105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/result.cpp
@@ -0,0 +1,67 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-05-17 00:34:09
+ *   Description   ：
+ */
+#include <algorithm>        //STL 通用算法
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+public:
+    /* from Preorder and Inorder Traversal */
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+
+        return helper(preorder,0,preorder.size(),inorder,0,inorder.size());
+    
+    }
+
+    TreeNode* helper(vector<int>& preorder,int i,int j,vector<int>& inorder,int ii,int jj)
+    {
+        // tree        8 4 5 3 7 3
+        // preorder    8 [4 3 3 7] [5]
+        // inorder     [3 3 4 7] 8 [5]
+
+        // 每次从 preorder 头部取一个值 mid，作为树的根节点
+        // 检查 mid 在 inorder 中 的位置，则 mid 前面部分将作为 树的左子树，右部分作为树的右子树
+
+        f(i >= j || ii >= j)
+            return NULL;
+
+        int mid = preorder[i];
+        auto f = find(inorder.begin() + ii,inorder.begin() + jj,mid);
+
+        int dis = f - inorder.begin() - ii;
+
+        TreeNode* root = new TreeNode(mid);
+        root -> left = helper(preorder,i + 1,i + 1 + dis,inorder,ii,ii + dis);
+        root -> right = helper(preorder,i + 1 + dis,j,inorder,ii + dis + 1,jj);
+        return root;
+    }
+};i
+
+
+
+
+int main(){
+    return 0;
+}
