e6a7778961b749271c787fc97bdb1f6470b647ce
theck17 <theck17@163.com>
2021-05-19 18:46:48 +0800

    106

---
+++ leetcode/0106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/result.cpp
@@ -0,0 +1,48 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-05-19 18:46:18
+ *   Description   ：
+ */
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *Tree(vector<int>& in, int x, int y,vector<int>& po,int a,int b){
+        if(x > y || a > b)return nullptr;
+        TreeNode *node = new TreeNode(po[b]);
+        int SI = x;  
+        while(node->val != in[SI])SI++;
+        node->left  = Tree(in,x,SI-1,po,a,a+SI-x-1);
+        node->right = Tree(in,SI+1,y,po,a+SI-x,b-1);
+        return node;
+    }
+    TreeNode* buildTree(vector<int>& in, vector<int>& po){
+        return Tree(in,0,in.size()-1,po,0,po.size()-1);
+    }
+};
+
+
+
+
+int main(){
+    return 0;
+}
