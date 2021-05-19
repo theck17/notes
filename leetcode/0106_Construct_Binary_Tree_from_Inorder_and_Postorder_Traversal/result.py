cec71435935fa062481edb3eab9690f5078d0fcc
theck17 <theck17@163.com>
2021-05-19 18:49:26 +0800

    106

---
+++ leetcode/0106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/result.py
@@ -0,0 +1,33 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-05-19 18:49:03
+# Description:
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # @param inorder, a list of integers
+    # @param postorder, a list of integers
+    # @return a tree node
+    # 12:00
+    def buildTree(self, inorder, postorder):
+        if not inorder or not postorder:
+            return None
+
+        root = TreeNode(postorder.pop())
+        inorderIndex = inorder.index(root.val)
+
+        root.right = self.buildTree(inorder[inorderIndex + 1:], postorder)
+        root.left = self.buildTree(inorder[:inorderIndex], postorder)
+
+        return root
+
+
+if __name__ == "__main__":
+    pass
