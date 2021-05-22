b4e794e07ae8268359362cbb142b373b15fc5620
theck17 <theck17@163.com>
2021-05-22 19:09:38 +0800

    120

---
+++ leetcode/0120_Triangle/result.cpp
@@ -0,0 +1,33 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-05-22 19:09:14
+ *   Description   ：
+ */
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int> > &triangle) 
+    {
+        vector<int> mini = triangle[triangle.size()-1];
+        for ( int i = triangle.size() - 2; i>= 0 ; --i )
+            for ( int j = 0; j < triangle[i].size() ; ++ j )
+                mini[j] = triangle[i][j] + min(mini[j],mini[j+1]);
+        return mini[0];
+    }
+};
+
+
+
+
+int main(){
+    return 0;
+}
