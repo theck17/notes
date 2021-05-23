d9ce60c4c661e32feb8dcd4e91dc75062ce7144b
theck17 <theck17@163.com>
2021-05-23 10:36:24 +0800

    120

--- leetcode/0120_Triangle/result.py
+++ leetcode/0120_Triangle/result.py
@@ -2,18 +2,19 @@
 # Author: C.K
 # Email: theck17@163.com
 # DateTime:2021-05-22 19:11:25
-# Description: 
+# Description:
+
+import os
+import sys
 
 
 class Solution:
     def minimumTotal(self, T: List[List[int]]) -> int:
-        for i in range(len(T)-2,-1,-1):
-            for j in range(len(T[i])-1,-1,-1):
-                T[i][j] += min(T[i+1][j], T[i+1][j+1])
+        for i in range(len(T) - 2, -1, -1):
+            for j in range(len(T[i]) - 1, -1, -1):
+                T[i][j] += min(T[i + 1][j], T[i + 1][j + 1])
         return T[0][0]
 
 
-
 if __name__ == "__main__":
-   pass
-
+    pass
