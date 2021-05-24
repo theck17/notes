0dda80e41c08d9afa034a8e594a55e8250548a05
theck17 <theck17@163.com>
2021-05-24 18:25:10 +0800

    126

---
+++ leetcode/0126_Word_Ladder_2/result.py
@@ -0,0 +1,37 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-05-24 18:24:44
+# Description:
+
+
+class Solution(object):
+    def findLadders(self, beginWord, endWord, wordList):
+
+        wordList = set(wordList)
+        res = []
+        layer = {}
+        layer[beginWord] = [[beginWord]]
+
+        while layer:
+            newlayer = collections.defaultdict(list)
+            for w in layer:
+                if w == endWord:
+                    res.extend(k for k in layer[w])
+                else:
+                    for i in range(len(w)):
+                        for c in 'abcdefghijklmnopqrstuvwxyz':
+                            neww = w[:i] + c + w[i + 1:]
+                            if neww in wordList:
+                                newlayer[neww] += [
+                                    j + [neww] for j in layer[w]
+                                ]
+
+            wordList -= set(newlayer.keys())
+            layer = newlayer
+
+        return res
+
+
+if __name__ == "__main__":
+    pass
