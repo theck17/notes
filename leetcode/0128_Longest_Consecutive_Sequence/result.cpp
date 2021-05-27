edcee1be5c607c7411bd07fe741b6d4409e67f41
theck17 <theck17@163.com>
2021-05-27 21:26:54 +0800

    128

---
+++ leetcode/0128_Longest_Consecutive_Sequence/result.cpp
@@ -0,0 +1,35 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-05-27 21:26:30
+ *   Description   ：
+ */
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+int longestConsecutive(const vector<int> &num) {
+    unordered_set<int> s(num.begin(), num.end()), searched;
+    int longest = 0;
+    for (int i: num) {
+        if (searched.find(i) != searched.end()) continue;
+        searched.insert(i);
+        int j = i - 1, k = i + 1;
+        while (s.find(j) != s.end()) searched.insert(j--);
+        while (s.find(k) != s.end()) searched.insert(k++);
+        longest = max(longest, k - 1 - j);
+    }
+    return longest;
+}
+
+
+
+
+int main(){
+    return 0;
+}
