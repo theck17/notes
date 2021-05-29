bb46022ad21e4980bfa59b3c93f2a4b21a6af896
theck17 <theck17@163.com>
2021-05-29 13:47:55 +0800

    152

---
+++ leetcode/0152_Maximum_Product_Subarray/result.cpp
@@ -0,0 +1,34 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-05-29 13:47:30
+ *   Description   ：
+ */
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+class Solution {
+public:
+   int maxProduct(vector<int> A) {
+        int n = A.size(), res = A[0], l = 0, r = 0;
+        for (int i = 0; i < n; i++) {
+            l =  (l ? l : 1) * A[i];
+            r =  (r ? r : 1) * A[n - 1 - i];
+            res = max(res, max(l, r));
+        }
+        return res;
+    }
+};
+
+
+
+
+int main(){
+    return 0;
+}
