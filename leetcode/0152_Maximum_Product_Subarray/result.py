ea8c1809480090e7d1197f4a4d514f4ea45c5ad7
theck17 <theck17@163.com>
2021-05-29 13:44:04 +0800

    152

---
+++ leetcode/0152_Maximum_Product_Subarray/result.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-05-29 13:43:51
+# Description:
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        B = nums[::-1]
+        for i in range(1, len(nums)):
+            nums[i] *= nums[i - 1] or 1
+            B[i] *= B[i - 1] or 1
+        return max(nums + B)
+
+
+if __name__ == "__main__":
+    pass
