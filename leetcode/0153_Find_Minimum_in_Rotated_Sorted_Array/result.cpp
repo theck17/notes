f9f3ee9f6b445a2eb9a4d115c95f04d4dfb24d98
theck17 <theck17@163.com>
2021-06-01 21:31:07 +0800

    153

---
+++ leetcode/0153_Find_Minimum_in_Rotated_Sorted_Array/result.cpp
@@ -0,0 +1,41 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-06-01 21:30:49
+ *   Description   ：
+ */
+#include <algorithm>        //STL 通用算法
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0,  right = nums.size() - 1;
+        while(left < right) {
+            if(nums[left] < nums[right]) 
+                return nums[left];
+
+            int mid = (left + right)/2;
+            if(nums[mid] > nums[right])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+
+        return nums[left];
+    }
+};
+
+
+
+
+int main(){
+    return 0;
+}
