c1dfe20a45b533930f1308eb997a4aa46cc3a640
theck17 <theck17@163.com>
2021-06-05 11:24:13 +0800

    169

---
+++ leetcode/0169_Majority_Element/result.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-06-05 11:23:41
+# Description:
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        numsa.sort()
+        return nums[len(nums) // 2]
+
+
+if __name__ == "__main__":
+    pass
