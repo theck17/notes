9d0b9dbb6de7eec9638b4b45ab88bfa957337d98
theck17 <theck17@163.com>
2021-06-07 18:41:00 +0800

    189

---
+++ leetcode/0189_Rotate_Array/result.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-06-07 18:40:42
+# Description:
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        if len(nums) > 1 and k > 0:
+            nums[:] = nums[-k:] + nums[:-k]
+
+
+if __name__ == "__main__":
+    pass
