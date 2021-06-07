327b5d780840665d6c9bbeccbd0365f09df93208
theck17 <theck17@163.com>
2021-06-07 18:42:03 +0800

    189

---
+++ leetcode/0189_Rotate_Array/result.cpp
@@ -0,0 +1,40 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-06-07 18:41:38
+ *   Description   ：
+ */
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+class Solution 
+{
+public:
+    void rotate(int nums[], int n, int k) 
+    {
+        for (; k = k%n; n -= k, nums += k)
+        {
+            // Swap the last k elements with the first k elements. 
+            // The last k elements will be in the correct positions
+            // but we need to rotate the remaining (n - k) elements 
+            // to the right by k steps.
+            for (int i = 0; i < k; i++)
+            {
+                swap(nums[i], nums[n - k + i]);
+            }
+        }
+    }
+};
+
+
+
+
+int main(){
+    return 0;
+}
