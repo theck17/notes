cce4bdad060fda431ec6d91f7d2478e108139586
theck17 <theck17@163.com>
2021-06-11 19:30:24 +0800

    216

---
+++ leetcode/0216_Combination_Sum_3/result.cpp
@@ -0,0 +1,44 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-06-11 19:29:54
+ *   Description   ：
+ */
+#include <algorithm>        //STL 通用算法
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+class Solution {
+public:
+  void combination(vector<vector<int>>& result, vector<int> sol, int k, int n) {
+    if (sol.size() == k && n == 0) { result.push_back(sol); return ; }
+    if (sol.size() < k) {
+      for (int i = sol.empty() ? 1 : sol.back() + 1; i <= 9; ++i) {
+        if (n - i < 0) break;
+        sol.push_back(i);
+        combination(result, sol, k, n - i);
+        sol.pop_back();
+      }
+    }
+  }
+
+  vector<vector<int>> combinationSum3(int k, int n) {
+    vector<vector<int>> result;
+    vector<int> sol;
+    combination(result, sol, k, n);
+    return result;
+  }
+};
+
+
+
+
+int main(){
+    return 0;
+}
