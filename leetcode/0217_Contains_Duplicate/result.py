dc65bd91e8de8de4ac42b6ec7a87294d841e650e
theck17 <theck17@163.com>
2021-06-16 20:04:38 +0800

    217

---
+++ leetcode/0217_Contains_Duplicate/result.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-06-16 20:04:15
+# Description: 
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
+if __name__ == "__main__":
+   pass
+
