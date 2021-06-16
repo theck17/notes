d3b4eaa29d00f8faabddc1e11a897d3d2568db2d
theck17 <theck17@163.com>
2021-06-16 20:16:12 +0800

    217

---
+++ leetcode/0217_Contains_Duplicate/result.cpp
@@ -0,0 +1,26 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-06-16 20:15:37
+ *   Description   ：
+ */
+#include <algorithm>        //STL 通用算法
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        return nums.size() > set<int>(nums.begin(), nums.end()).size();        
+    }
+};
+
+int main(){
+    return 0;
+}
