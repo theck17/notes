fe05c88186f3f0fbadd277b9b1a70728cb672351
theck17 <theck17@163.com>
2021-06-28 19:59:54 +0800

    136

---
+++ leetcode/0136_Single_Number/result.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-06-28 19:59:25
+# Description:
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dic = {}
+        for num in nums:
+            dic[num] = dic.get(num, 0) + 1
+        for key, val in dic.items():
+            if val == 1:
+                return key
+
+
+if __name__ == "__main__":
+    pass
