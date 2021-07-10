51ea8662a040d72aaaca7b8933b9612cf39136a7
theck17 <theck17@163.com>
2021-07-10 16:20:05 +0800

    58

---
+++ leetcode/0058_Length_of_Last_Word/result.cpp
@@ -0,0 +1,30 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-07-10 16:19:30
+ *   Description   ：
+ */
+#include <algorithm>        //STL 通用算法
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0, tail = s.length() - 1;
+        while (tail >= 0 && s[tail] == ' ') tail--;
+        while (tail >= 0 && s[tail] != ' ') {
+            len++;
+            tail--;
+        }
+        return len;
+    }
+};
+
+int main(){
+    return 0;
+}
