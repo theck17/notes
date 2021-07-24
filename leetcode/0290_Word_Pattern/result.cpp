904a1b20d63f10637fa0353f0b966f677a414808
theck17 <theck17@163.com>
2021-07-24 21:33:16 +0800

    290

---
+++ leetcode/0290_Word_Pattern/result.cpp
@@ -0,0 +1,32 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-07-24 21:23:38
+ *   Description   ：
+ */
+#include <algorithm>        //STL 通用算法
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) {
+        map<char, int> p2i;
+        map<string, int> w2i;
+        istringstream in(str);
+        int i = 0, n = pattern.size();
+        for (string word; in >> word; ++i) {
+            if (i == n || p2i[pattern[i]] != w2i[word])
+                return false;
+            p2i[pattern[i]] = w2i[word] = i + 1;
+        }
+        return i == n;
+    }
+};
+
+int main(){
+    return 0;
+}
---
+++ leetcode/0290_Word_Pattern/result.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-07-24 21:28:54
+# Description: 
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        # split str into single words.
+        slist = s.split()
+        
+        # here we need to consider the len difference
+        # eg: pattern:"aaa" str:"aa aa aa aa"
+        if len(pattern) != len(slist):
+            return False
+        
+        # just like we do in isomorphic problem.
+        return (len(set(pattern)) == len(set(slist)) == len(set(zip(pattern, slist))))
+
+if __name__ == "__main__":
+   pass
+
