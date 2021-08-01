37583b44cefba33dc60fb5283b72400f22f08eaf
theck17 <theck17@163.com>
2021-08-01 20:03:12 +0800

    67

---
+++ leetcode/0067_Add_Binary/result.cpp
@@ -0,0 +1,38 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-08-01 20:02:42
+ *   Description   ：
+ */
+#include <string>          //字符串类
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+
+class Solution
+{
+public:
+    string addBinary(string a, string b)
+    {
+        string s = "";
+        
+        int c = 0, i = a.size() - 1, j = b.size() - 1;
+        while(i >= 0 || j >= 0 || c == 1)
+        {
+            c += i >= 0 ? a[i --] - '0' : 0;
+            c += j >= 0 ? b[j --] - '0' : 0;
+            s = char(c % 2 + '0') + s;
+            c /= 2;
+        }
+        
+        return s;
+    }
+};
+
+int main(){
+    return 0;
+}
