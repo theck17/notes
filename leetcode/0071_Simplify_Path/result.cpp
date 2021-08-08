137ef316a9c4a2c38c18de7817314ea51e092d71
theck17 <theck17@163.com>
2021-08-08 18:14:21 +0800

    71

---
+++ leetcode/0071_Simplify_Path/result.cpp
@@ -0,0 +1,32 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-08-08 18:13:25
+ *   Description   ：
+ */
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string res, tmp;
+        vector<string> stk;
+        stringstream ss(path);
+        while(getline(ss,tmp,'/')) {
+            if (tmp == "" or tmp == ".") continue;
+            if (tmp == ".." and !stk.empty()) stk.pop_back();
+            else if (tmp != "..") stk.push_back(tmp);
+        }
+        for(auto str : stk) res += "/"+str;
+        return res.empty() ? "/" : res;
+    }
+};
+
+int main(){
+    return 0;
+}
