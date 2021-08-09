edf727e72223ffca195dd36616d8dd1f3310a065
theck17 <theck17@163.com>
2021-08-09 19:44:18 +0800

    12

---
+++ leetcode/0012_Integer_to_Roman/result.cpp
@@ -0,0 +1,37 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-08-09 19:43:58
+ *   Description   ：
+ */
+#include <vector>          //STL 动态数组容器
+#include <valarray>        //对包含值的数组的操作
+#include <ctime>           //定义关于时间的函数
+using namespace std;
+class Solution {
+public:
+    string intToRoman(int num) 
+    {
+        string res;
+        string sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        
+        for(int i=0; num != 0; i++)
+        {
+            while(num >= val[i])
+            {
+                num -= val[i];
+                res += sym[i];
+            }
+        }
+        
+        return res;
+    }
+};
+
+int main(){
+    return 0;
+}
