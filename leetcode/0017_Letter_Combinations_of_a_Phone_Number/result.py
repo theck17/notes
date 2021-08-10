76eeeaa733e2315a2b4dd3b03301a0caa8881bbb
theck17 <theck17@163.com>
2021-08-10 21:46:45 +0800

    17

---
+++ leetcode/0017_Letter_Combinations_of_a_Phone_Number/result.py
@@ -0,0 +1,28 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-08-10 21:45:28
+# Description:
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        mapping = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        if len(digits) == 0:
+            return []
+        if len(digits) == 1:
+            return list(mapping[digits[0]])
+        prev = self.letterCombinations(digits[:-1])
+        additional = mapping[digits[-1]]
+        return [s + c for s in prev for c in additional]
+
+
+if __name__ == "__main__":
+    pass
