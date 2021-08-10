91bb58a63d7de019d39739a276ff3cd5f411f073
theck17 <theck17@163.com>
2021-08-10 21:44:52 +0800

    17

---
+++ leetcode/0017_Letter_Combinations_of_a_Phone_Number/result.cpp
@@ -0,0 +1,39 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-08-10 21:44:28
+ *   Description   ：
+ */
+#include <algorithm>        //STL 通用算法
+using namespace std;
+class Solution {
+public:
+    const vector<string> pad = {
+        "", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"
+    };
+    
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+		vector<string> result;
+        result.push_back("");
+        
+        for(auto digit: digits) {
+            vector<string> tmp;
+            for(auto candidate: pad[digit - '0']) {
+                for(auto s: result) {
+                    tmp.push_back(s + candidate);
+                }
+            }
+            result.swap(tmp);
+        }
+        return result;
+    }
+};
+
+int main(){
+    return 0;
+}
