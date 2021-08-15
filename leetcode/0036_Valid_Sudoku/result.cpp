37d3668288bfe49b2e81fbc48cad832f4d39d6e6
theck17 <theck17@163.com>
2021-08-15 10:18:45 +0800

    36

---
+++ leetcode/0036_Valid_Sudoku/result.cpp
@@ -0,0 +1,35 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-08-15 10:18:20
+ *   Description   ：
+ */
+#include <algorithm>        //STL 通用算法
+using namespace std;
+class Solution
+{
+public:
+    bool isValidSudoku(vector<vector<char> > &board)
+    {
+        int used1[9][9] = {0}, used2[9][9] = {0}, used3[9][9] = {0};
+        
+        for(int i = 0; i < board.size(); ++ i)
+            for(int j = 0; j < board[i].size(); ++ j)
+                if(board[i][j] != '.')
+                {
+                    int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
+                    if(used1[i][num] || used2[j][num] || used3[k][num])
+                        return false;
+                    used1[i][num] = used2[j][num] = used3[k][num] = 1;
+                }
+        
+        return true;
+    }
+};
+
+int main(){
+    return 0;
+}
