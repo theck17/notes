5f50a308b713569af69758517a48bb5072941e8f
theck17 <theck17@163.com>
2021-08-18 21:58:47 +0800

    141

---
+++ leetcode/0141_Linked_List_Cycle/result.cpp
@@ -0,0 +1,42 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-08-18 21:56:03
+ *   Description   ：
+ */
+#include <algorithm>        //STL 通用算法
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (fast && fast->next) {
+            
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+};
+
+int main(){
+    return 0;
+}
