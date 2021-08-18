00b40f7c49704f39ee8284227099fe859f391016
theck17 <theck17@163.com>
2021-08-18 21:59:50 +0800

    141

---
+++ leetcode/0141_Linked_List_Cycle/result.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-08-18 21:59:33
+# Description:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast: return True
+
+        return False
+
+
+if __name__ == "__main__":
+    pass
