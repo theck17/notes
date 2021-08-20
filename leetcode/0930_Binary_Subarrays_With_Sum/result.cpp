22612f7eb6cee01ece51b2ad48789b5bafde5a37
theck17 <theck17@163.com>
2021-08-20 19:09:47 +0800

    930

---
+++ leetcode/0930_Binary_Subarrays_With_Sum/result.cpp
@@ -0,0 +1,28 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-08-20 19:09:19
+ *   Description   ：
+ */
+#include <algorithm>        //STL 通用算法
+using namespace std;
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& A, int S) {
+        unordered_map<int, int> c({{0, 1}});
+        int psum = 0, res = 0;
+        for (int i : A) {
+            psum += i;
+            res += c[psum - S];
+            c[psum]++;
+        }
+        return res;
+    }
+};
+
+int main(){
+    return 0;
+}
