3052e4df937f11fee49b35bd84b8cddfc57713cc
theck17 <theck17@163.com>
2021-08-25 21:40:45 +0800

    709

--- leetcode/0709_To_Lower_Case/result.py
+++ leetcode/0709_To_Lower_Case/result.py
@@ -5,8 +5,7 @@
 # Description:
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return "".join(
-            chr(ord(c) + 32) if 65 <= ord(c) <= 90 else c for c in str)
+        return "".join(chr(ord(c) + 32) if "A" <= c <= "Z" else c for c in s)
 
 
 if __name__ == "__main__":
