8842b21bedc5350accd5df229dca0b223fc9b719
theck17 <theck17@163.com>
2021-08-30 22:23:05 +0800

    389

---
+++ leetcode/0389_Find_the_Difference/result.cpp
@@ -0,0 +1,24 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-08-30 22:22:43
+ *   Description   ：
+ */
+#include <algorithm>        //STL 通用算法
+using namespace std;
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char r=0;
+        for(char c:s) r ^=c;
+        for(char c:t) r ^=c;
+        return r;
+    }
+};
+
+int main(){
+    return 0;
+}
