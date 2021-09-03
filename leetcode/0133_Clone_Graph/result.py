748b34b2f6df578be0f7790833b5eb946833df8b
theck17 <theck17@163.com>
2021-09-03 16:23:19 +0800

    133

---
+++ leetcode/0133_Clone_Graph/result.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-09-03 16:21:08
+# Description:
+
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+        m = {node: Node(node.val)}
+        stack = [node]
+        while stack:
+            n = stack.pop()
+            for neigh in n.neighbors:
+                if neigh not in m:
+                    stack.append(neigh)
+                    m[neigh] = Node(neigh.val)
+                m[n].neighbors.append(m[neigh])
+        return m[node]
+
+
+if __name__ == "__main__":
+    pass
