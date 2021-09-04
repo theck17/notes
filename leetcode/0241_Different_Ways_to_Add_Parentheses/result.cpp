8b92f57218abd460f2909347e283406e4b186009
theck17 <theck17@163.com>
2021-09-04 17:28:38 +0800

    241

---
+++ leetcode/0241_Different_Ways_to_Add_Parentheses/result.cpp
@@ -0,0 +1,43 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-09-04 17:28:14
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string input) {
+        vector<int> result;
+        int size = input.size();
+        for (int i = 0; i < size; i++) {
+            char cur = input[i];
+            if (cur == '+' || cur == '-' || cur == '*') {
+                // Split input string into two parts and solve them recursively
+                vector<int> result1 = diffWaysToCompute(input.substr(0, i));
+                vector<int> result2 = diffWaysToCompute(input.substr(i+1));
+                for (auto n1 : result1) {
+                    for (auto n2 : result2) {
+                        if (cur == '+')
+                            result.push_back(n1 + n2);
+                        else if (cur == '-')
+                            result.push_back(n1 - n2);
+                        else
+                            result.push_back(n1 * n2);    
+                    }
+                }
+            }
+        }
+        // if the input string contains only number
+        if (result.empty())
+            result.push_back(atoi(input.c_str()));
+        return result;
+    }
+};
+
+int main(){
+    return 0;
+}
