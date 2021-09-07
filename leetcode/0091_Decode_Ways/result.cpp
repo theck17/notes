a5b35237f27cb95a8c7870a6eda3f1d076461d19
theck17 <theck17@163.com>
2021-09-07 20:41:16 +0800

    91

--- leetcode/0091_Decode_Ways/result.cpp
+++ leetcode/0091_Decode_Ways/result.cpp
@@ -8,25 +8,24 @@
  *   Description   ：
  */
 using namespace std;
+
+
 class Solution {
 public:
     int numDecodings(string s) {
-        int ans=0;
-        ans = recur(s,0);
-        return ans;
-    }
-    int recur(string s, int pos){
-        if(pos==s.size())return 1;
-        if(s[pos]=='0')return 0;
-        if(pos==s.size()-1)return 1;
-        string b = s.substr(pos,2);
-        
-        int way1 = recur(s,pos+1);
-        int way2=0;
-        if(stoi(b)<=26&&stoi(b)>0){
-            way2 = recur(s,pos+2);
+        vector<int> dp(s.size()+1);
+        dp[0]=1;
+        if(s[0]=='0')dp[1]=0;
+        else dp[1]=1;
+        for(int i=2;i<=s.size();i++){
+            int way1,way2;
+            if(s[i-1]=='0')way1=0;
+            else way1=dp[i-1];
+            if(stoi(s.substr(i-2,2))<=26&&stoi(s.substr(i-2,2))>0&&s[i-2]!='0')way2=dp[i-2];
+            else way2=0;
+            dp[i]=way1+way2;
         }
-        return way1+way2;
+        return dp[s.size()];
     }
 };
 
