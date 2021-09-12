bdb09ff6299396d9845426b295b9aaba6bb41147
theck17 <theck17@163.com>
2021-09-12 11:56:40 +0800

    151

---
+++ leetcode/0151_Reverse_Words_in_a_String/result.cpp
@@ -0,0 +1,36 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-09-12 11:56:19
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+string reverseWords(string s) {
+        if(s.size() == 0) return s;
+        stack<string> stack;
+        string result;
+        for(int i=0; i<s.size(); i++) {
+            string word;
+            if(s[i]==' ') continue; //skip spaces
+            while(i<s.size() && s[i]!=' ' ) { //store continuous letters into word
+                word += s[i]; i++;
+            }
+            stack.push(word); //push word to the stack
+        }
+        while(!stack.empty()) {
+            result += stack.top(); stack.pop();
+            if(!stack.empty()) result += " ";
+        }
+        return result;
+    }
+};
+
+
+int main(){
+    return 0;
+}
