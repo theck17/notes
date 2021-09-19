27dce46811334b55fac484d492f154d8c07e48b2
theck17 <theck17@163.com>
2021-09-19 13:34:57 +0800

    347

---
+++ leetcode/0347_Top_K_Frequent_Elements/result.cpp
@@ -0,0 +1,36 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-09-19 13:32:28
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        for (int num : nums)
+            ++m[num];
+        
+        vector<vector<int>> buckets(nums.size() + 1); 
+        for (auto p : m)
+            buckets[p.second].push_back(p.first);
+        
+        vector<int> ans;
+        for (int i = buckets.size() - 1; i >= 0 && ans.size() < k; --i) {
+            for (int num : buckets[i]) {
+                ans.push_back(num);
+                if (ans.size() == k)
+                    break;
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    return 0;
+}
