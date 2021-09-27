b9c2ff911ec203de14a96e4de50bb3967eb9a59b
theck17 <theck17@163.com>
2021-09-27 19:45:14 +0800

    406

---
+++ leetcode/0406_Queue_Reconstruction_by_Height/result.cpp
@@ -0,0 +1,28 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-09-27 19:44:49
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), cmp);
+        vector<vector<int>> res;
+        for(int i = 0; i < people.size(); i++){
+            res.insert(res.begin()+people[i][1], people[i]);
+        }
+        return res;
+    }
+    static bool cmp(vector<int>& p1, vector<int>& p2){
+        if(p1[0]!=p2[0]) return p1[0]>p2[0];
+        else return p1[1]<p2[1];
+    }
+};
+int main(){
+    return 0;
+}
