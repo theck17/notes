1b5db15d4c0fbb66bde7f073aa6c060141ef258f
theck17 <theck17@163.com>
2021-10-16 19:54:36 +0800

    300

---
+++ leetcode/0300_Longest_Increasing_Subsequence/result.cpp
@@ -0,0 +1,29 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-10-16 19:53:32
+ *   Description   ：
+ */
+using namespace std;
+class Solution { 
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> sub;
+        for (int x : nums) {
+            if (sub.empty() || sub[sub.size() - 1] < x) {
+                sub.push_back(x);
+            } else {
+                auto it = lower_bound(sub.begin(), sub.end(), x);
+                *it = x; 
+            }
+        }
+        return sub.size();
+    }
+};
+
+int main(){
+    return 0;
+}
