61cad132e6f1d6cb79860cb16011bfb54684063e
theck17 <theck17@163.com>
2021-10-21 20:39:02 +0800

    94

---
+++ leetcode/0094_Binary_Tree_Inorder_Traversal/result.cpp
@@ -0,0 +1,40 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-10-21 20:37:52
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        while (root) {
+            if (root -> left) {
+                TreeNode* pre = root -> left;
+                while (pre -> right && pre -> right != root) {
+                    pre = pre -> right;
+                }
+                if (!pre -> right) {
+                    pre -> right = root;
+                    root = root -> left;
+                } else {
+                    pre -> right = NULL;
+                    nodes.push_back(root -> val);
+                    root = root -> right;
+                }
+            } else {
+                nodes.push_back(root -> val);
+                root = root -> right;
+            }
+        }
+        return nodes;
+    }
+};
+
+int main(){
+    return 0;
+}
