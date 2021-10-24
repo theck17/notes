6e0f773fb186cdd08af1aa7c731f4a163ac1efab
theck17 <theck17@163.com>
2021-10-24 12:33:23 +0800

    287

--- leetcode/0287_Find_the_Duplicate_Number/result.py
+++ leetcode/0287_Find_the_Duplicate_Number/result.py
@@ -5,20 +5,16 @@
 # Description:
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        low = 0
-        high = len(nums) - 1
-        mid = (high + low) / 2
-        while high - low > 1:
-            count = 0
-            for k in nums:
-                if mid < k <= high:
-                    count += 1
-            if count > high - mid:
-                low = mid
+        beg, end = 1, len(nums) - 1
+        while beg + 1 <= end:
+            mid, count = (beg + end) // 2, 0
+            for num in nums:
+                if num <= mid: count += 1
+            if count <= mid:
+                beg = mid + 1
             else:
-                high = mid
-            mid = (high + low) / 2
-        return high
+                end = mid
+        return end
 
 
 if __name__ == "__main__":
