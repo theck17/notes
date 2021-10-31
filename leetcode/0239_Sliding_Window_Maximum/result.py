f5c82e46adf98f0877b5518e81d59314cb5fb84a
theck17 <theck17@163.com>
2021-10-31 11:54:55 +0800

    239

---
+++ leetcode/0239_Sliding_Window_Maximum/result.py
@@ -0,0 +1,35 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-10-31 11:54:44
+# Description:
+from collections import deque
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q = deque()
+        result = []
+
+        for i in range(len(nums)):
+            # the first/left (max) element is out of the current window
+            if q and i - q[0] == k:
+                q.popleft()
+
+            while q:
+                # pop useles elements from last/right of the queue
+                if nums[q[-1]] < nums[i]:
+                    q.pop()
+                else:
+                    break
+
+            q.append(i)
+
+            if i >= k - 1: # i == k-1 is the beginning of a full window
+                result.append(nums[q[0]])
+
+        return result
+
+
+if __name__ == "__main__":
+    pass
