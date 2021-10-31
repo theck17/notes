5917f6242bf0c1688f084b45994107ca9a523cba
theck17 <theck17@163.com>
2021-10-31 11:53:57 +0800

    239

---
+++ leetcode/0239_Sliding_Window_Maximum/result.cpp
@@ -0,0 +1,29 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-10-31 11:53:14
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> ans;
+        for (int i=0; i<nums.size(); i++) {
+            if (!dq.empty() && dq.front() == i-k) dq.pop_front();
+            while (!dq.empty() && nums[dq.back()] < nums[i])
+                dq.pop_back();
+            dq.push_back(i);
+            if (i>=k-1) ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
+
+int main(){
+    return 0;
+}
