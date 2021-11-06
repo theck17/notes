45b58728348a53e4252bfa72bc9f9fe2a7b3df27
theck17 <theck17@163.com>
2021-11-06 14:07:52 +0800

    134

---
+++ leetcode/0134_Gas_Station/result.cpp
@@ -0,0 +1,34 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-11-06 14:06:41
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        int i, j, n = gas.size();
+
+        for (i = 0; i < n; i += j) {
+            int gas_left = 0;
+            for (j = 1; j <= n; j++) {
+                int k = (i + j - 1) % n;
+                gas_left += gas[k] - cost[k];
+                if (gas_left < 0)
+                    break;
+            }
+            if (j > n)
+                return i;
+        }
+
+        return -1;
+    }
+};
+
+int main(){
+    return 0;
+}
