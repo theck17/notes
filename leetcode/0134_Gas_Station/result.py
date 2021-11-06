e1444df77c4bffd1732584b55cd7f258c7e57873
theck17 <theck17@163.com>
2021-11-06 14:08:33 +0800

    134

---
+++ leetcode/0134_Gas_Station/result.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-11-06 14:07:56
+# Description:
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        diffs = [a - b for a, b in zip(gas, cost)]
+        n = len(diffs)
+        cumsum, out = 0, 0
+        for i in range(2 * n):
+            cumsum += diffs[i % n]
+            if cumsum < 0:
+                cumsum = 0
+                out = i + 1
+
+        return -1 if out > n else out
+
+
+if __name__ == "__main__":
+    pass
