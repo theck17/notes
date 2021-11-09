eb64eab60d797c8a682eddada178d43b8f24a1d5
theck17 <theck17@163.com>
2021-11-09 21:27:03 +0800

    463

---
+++ leetcode/0463_Island_Perimeter/result.cpp
@@ -0,0 +1,33 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-11-09 21:25:46
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int count=0, repeat=0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0; j<grid[i].size();j++)
+                {
+                    if(grid[i][j]==1)
+                    {
+                        count ++;
+                        if(i!=0 && grid[i-1][j] == 1) repeat++;
+                        if(j!=0 && grid[i][j-1] == 1) repeat++;
+                    }
+                }
+        }
+        return 4*count-repeat*2;
+    }   
+};
+
+int main(){
+    return 0;
+}
