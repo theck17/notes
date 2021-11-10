95d108fc3fadb07c8193ac3b690060f7b09dc1f4
theck17 <theck17@163.com>
2021-11-10 19:45:33 +0800

    486

---
+++ leetcode/0486_Predict_the_Winner/result.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-11-10 19:44:47
+# Description:
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        def helper(i, j):
+            if i == j:
+                return nums[i]
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            score = max(nums[j] - helper(i, j - 1), nums[i] - helper(i + 1, j))
+            memo[(i, j)] = score
+
+            return score
+
+        memo = {}
+        return helper(0, len(nums) - 1) >= 0
+
+
+if __name__ == "__main__":
+    pass
