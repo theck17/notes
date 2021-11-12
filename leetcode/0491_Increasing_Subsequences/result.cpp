aa19a6c3514163953a514bb3d7d1b87995f725b0
theck17 <theck17@163.com>
2021-11-12 20:01:23 +0800

    491

---
+++ leetcode/0491_Increasing_Subsequences/result.cpp
@@ -0,0 +1,36 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-11-12 20:00:08
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> seq;
+        dfs(res, seq, nums, 0);
+        return res;
+    }
+    
+    void dfs(vector<vector<int>>& res, vector<int>& seq, vector<int>& nums, int pos) {
+        if(seq.size() > 1) res.push_back(seq);
+        unordered_set<int> hash;
+        for(int i = pos; i < nums.size(); ++i) {
+            if((seq.empty() || nums[i] >= seq.back()) && hash.find(nums[i]) == hash.end()) {
+                seq.push_back(nums[i]);
+                dfs(res, seq, nums, i + 1);
+                seq.pop_back();
+                hash.insert(nums[i]);
+            }
+        }
+    }
+};
+
+int main(){
+    return 0;
+}
