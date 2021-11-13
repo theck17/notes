fc6f3a681044a594680072db1663a560b0cc5361
theck17 <theck17@163.com>
2021-11-13 14:43:21 +0800

    397

---
+++ leetcode/0397_Integer_Replacement/result.cpp
@@ -0,0 +1,31 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-11-13 14:40:35
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+private:
+    unordered_map<int, int> visited;
+
+public:
+    int integerReplacement(int n) {        
+        if (n == 1) { return 0; }
+        if (visited.count(n) == 0) {
+            if (n & 1 == 1) {
+                visited[n] = 2 + min(integerReplacement(n / 2), integerReplacement(n / 2 + 1));
+            } else {
+                visited[n] = 1 + integerReplacement(n / 2);
+            }
+        }
+        return visited[n];
+    }
+};
+
+int main(){
+    return 0;
+}
