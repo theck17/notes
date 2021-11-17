bd2e339a793c094a9da8b9c1468127e690588945
theck17 <theck17@163.com>
2021-11-17 19:41:04 +0800

    526

---
+++ leetcode/0526_Beautiful_Arrangement/result.cpp
@@ -0,0 +1,34 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-11-17 19:40:02
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    int countArrangement(int N) {
+        vector<int> vs;
+        for (int i=0; i<N; ++i) vs.push_back(i+1);
+        return counts(N, vs);
+    }
+    int counts(int n, vector<int>& vs) {
+        if (n <= 0) return 1;
+        int ans = 0;
+        for (int i=0; i<n; ++i) {
+            if (vs[i]%n==0 || n%vs[i]==0) {
+                swap(vs[i], vs[n-1]);
+                ans += counts(n-1, vs);
+                swap(vs[i], vs[n-1]);
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    return 0;
+}
