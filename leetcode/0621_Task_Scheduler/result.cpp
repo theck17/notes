b19c88697f7bd2457119377ab4a8e933818a0a71
theck17 <theck17@163.com>
2021-11-20 12:00:32 +0800

    621

---
+++ leetcode/0621_Task_Scheduler/result.cpp
@@ -0,0 +1,30 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-11-20 11:59:00
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char,int>mp;
+        int count = 0;
+        for(auto e : tasks)
+        {
+            mp[e]++;
+            count = max(count, mp[e]);
+        }
+        
+        int ans = (count-1)*(n+1);
+        for(auto e : mp) if(e.second == count) ans++;
+        return max((int)tasks.size(), ans);
+    }
+};
+
+int main(){
+    return 0;
+}
