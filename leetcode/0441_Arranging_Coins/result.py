0c357e650b111dbdab958cbd218eeb633e655b51
theck17 <theck17@163.com>
2021-11-21 10:37:52 +0800

    441

---
+++ leetcode/0441_Arranging_Coins/result.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-11-21 10:37:21
+# Description:
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        k = total = 1
+        while total <= n:
+            k += 1
+            total += k
+        return k - 1
+
+
+if __name__ == "__main__":
+    pass
