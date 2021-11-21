36e1df2e587188092bf74476258f5bbc32fc103a
theck17 <theck17@163.com>
2021-11-21 10:37:17 +0800

    441

---
+++ leetcode/0441_Arranging_Coins/result.c
@@ -0,0 +1,18 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.c
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-11-21 10:37:01
+ *   Description   ：
+ */
+int arrangeCoins(int n) {
+    long ans = n;
+    ans = sqrt(ans * 8 + 1) - 1; 
+    return ans >> 1;
+}
+
+int main(){
+    return 0;
+}
