0aff5000bb752f728e94934f2ec7ca10ffa06255
theck17 <theck17@163.com>
2021-11-24 21:26:14 +0800

    234

---
+++ leetcode/0234_Palindrome_Linked_List/result.cpp
@@ -0,0 +1,29 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-11-24 21:22:20
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    ListNode* temp;
+    bool isPalindrome(ListNode* head) {
+        temp = head;
+        return check(head);
+    }
+    
+    bool check(ListNode* p) {
+        if (NULL == p) return true;
+        bool isPal = check(p->next) & (temp->val == p->val);
+        temp = temp->next;
+        return isPal;
+    }
+};
+
+int main(){
+    return 0;
+}
