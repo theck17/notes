4c34c912b4915b832113d444e48f476eb49fc9fd
theck17 <theck17@163.com>
2021-11-25 21:20:32 +0800

    438

---
+++ leetcode/0438_Find_All_Anagrams_in_a_String/result.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-11-25 21:19:34
+# Description:
+from collections import Counter
+
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        res = []
+        pCounter = Counter(p)
+        sCounter = Counter(s[:len(p) - 1])
+        for i in range(len(p) - 1, len(s)):
+            sCounter[s[i]] += 1 # include a new char in the window
+            if sCounter == pCounter: # This step is O(1), since there are at most 26 English letters
+                res.append(i - len(p) + 1) # append the starting index
+            sCounter[
+                s[i - len(p) +
+                  1]] -= 1 # decrease the count of oldest char in the window
+            if sCounter[s[i - len(p) + 1]] == 0:
+                del sCounter[s[i - len(p) + 1]] # remove the count if it is 0
+        return res
+
+
+if __name__ == "__main__":
+    pass
