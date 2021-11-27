016d78d20413bf4dd0415a0d0a5abc40f5c95453
theck17 <theck17@163.com>
2021-11-27 19:19:51 +0800

    912

---
+++ leetcode/0912_Sort_an_Array/result.cpp
@@ -0,0 +1,45 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-11-27 19:18:01
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    
+    int partition(vector<int> &nums, int l, int r){
+        int random = l + rand() % (r - l);
+        swap(nums[r], nums[random]);
+        int pivot = nums[r];
+        int sep=l-1;
+        for(int i=l; i<r; i++){
+            if(nums[i]<pivot){
+                sep++;
+                swap(nums[i], nums[sep]);
+            }
+        }
+        swap(nums[sep+1], nums[r]);
+        return sep+1;
+    }
+    
+    void quick_sort(vector<int> &nums, int l, int r){
+        if(l>=r) return;
+        
+        int q = partition(nums, l, r);
+        quick_sort(nums, l, q-1);
+        quick_sort(nums, q+1, r);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        srand(time(NULL));
+        quick_sort(nums, 0, nums.size()-1);
+        return nums;
+    }
+};
+
+int main(){
+    return 0;
+}
