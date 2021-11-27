60e31237d3d7a0f4f983874b769ba76c0ea36635
theck17 <theck17@163.com>
2021-11-27 19:23:14 +0800

    912

--- leetcode/0912_Sort_an_Array/result.py
+++ leetcode/0912_Sort_an_Array/result.py
@@ -3,27 +3,25 @@
 # Email: theck17@163.com
 # DateTime:2021-11-27 19:15:37
 # Description:
+
+
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        l = len(nums)
-        if l > 1:
-            m = l // 2
-            L = nums[:m]
-            R = nums[m:]
-            self.sortArray(L)
-            self.sortArray(R)
+        def mergesort(A):
+            LA = len(A)
+            if LA == 1: return A
+            LH, RH = mergesort(A[:LA // 2]), mergesort(A[LA // 2:])
+            return merge(LH, RH)
 
-            # merge two sorted list
-            i = j = k = 0
-            while i < len(L) and j < len(R):
-                if L[i] < R[j]:
-                    nums[k] = L[i]
-                    i += 1
-                else:
-                    nums[k] = R[j]
-                    j += 1
-                k += 1
-            nums[k:] = L[i:] + R[j:]
+        def merge(LH, RH):
+            LLH, LRH = len(LH), len(RH)
+            S, i, j = [], 0, 0
+            while i < LLH and j < LRH:
+                if LH[i] <= RH[j]: i, _ = i + 1, S.append(LH[i])
+                else: j, _ = j + 1, S.append(RH[j])
+            return S + (RH[j:] if i == LLH else LH[i:])
+
+        return mergesort(nums)
 
 
 if __name__ == "__main__":
