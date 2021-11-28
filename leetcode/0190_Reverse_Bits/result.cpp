e91e2c90cdd54e69df321a42f505f70607ff3073
theck17 <theck17@163.com>
2021-11-28 10:04:13 +0800

    190

---
+++ leetcode/0190_Reverse_Bits/result.cpp
@@ -0,0 +1,25 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-11-28 10:03:14
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        n = (n >> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        return n;
+    }
+};
+
+int main(){
+    return 0;
+}
