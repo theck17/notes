841ec11386f7d627e991b49cc8b690df4291cf53
theck17 <theck17@163.com>
2021-12-04 09:47:40 +0800

    720

---
+++ leetcode/0720_Longest_Word_in_Dictionary/result.cpp
@@ -0,0 +1,29 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-12-04 09:47:06
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+        sort(words.begin(), words.end());
+        unordered_set<string> built;
+        string res;
+        for (string w : words) {
+            if (w.size() == 1 || built.count(w.substr(0, w.size() - 1))) {
+                res = w.size() > res.size() ? w : res;
+                built.insert(w);
+            }
+        }
+        return res;
+    }
+};
+
+int main(){
+    return 0;
+}
