f17f26cebe64a67466a6d870b7e55a8b288e8cb6
theck17 <theck17@163.com>
2021-12-06 19:35:10 +0800

    145

---
+++ leetcode/0145_Binary_Tree_Postorder_Traversal/result.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-12-06 19:31:28
+# Description:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res, stack = [], [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+        return res[::-1]
+
+
+if __name__ == "__main__":
+    pass
