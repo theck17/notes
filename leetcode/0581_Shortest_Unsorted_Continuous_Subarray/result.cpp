e078a1274ea43bdb5cf3cd0daa6fceb24c44eb00
theck17 <theck17@163.com>
2021-12-07 21:42:06 +0800

    581

---
+++ leetcode/0581_Shortest_Unsorted_Continuous_Subarray/result.cpp
@@ -0,0 +1,29 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-12-07 21:41:19
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        vector<int> sorted(nums);
+        sort(sorted.begin(), sorted.end());
+        int n = nums.size(), i = 0, j = n - 1;
+        while (i < n && nums[i] == sorted[i]) {
+            i++;
+        }
+        while (j > i && nums[j] == sorted[j]) {
+            j--;
+        }
+        return j + 1 - i;
+    }
+};
+
+int main(){
+    return 0;
+}
