f5d613e15499f54e895312a49513f075e553b630
theck17 <theck17@163.com>
2021-12-08 18:55:53 +0800

    229

---
+++ leetcode/0229_Majority_Element_2/result.cpp
@@ -0,0 +1,46 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-12-08 18:55:18
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> result;
+        
+        int count1=0, count2=0;
+        int result1=INT_MAX, result2=INT_MAX;
+        
+        for(int num:nums){
+            if(num==result1) count1++;
+            else if(num==result2) count2++;
+            else if(count1==0) { result1=num; count1=1; }
+            else if(count2==0) { result2=num; count2=1; }
+            else { count1--; count2--; }
+        }
+        
+        count1=count2=0;
+        for(int num:nums){
+            if(num==result1) count1++;
+            else if(num==result2)  count2++;
+        }
+        
+        cout<<result1<<":"<<count1<<endl;
+        cout<<result2<<":"<<count2<<endl;
+        
+        if(count1>n/3) result.push_back(result1);
+        if(count2>n/3) result.push_back(result2);
+        
+        return result;
+    }
+};
+
+int main(){
+    return 0;
+}
