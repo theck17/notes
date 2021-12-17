b23fd15252a5770f5af0c5e40e999bb3be3f6ed4
theck17 <theck17@163.com>
2021-12-17 19:36:38 +0800

    283

---
+++ leetcode/0283_Move_Zeroes/result.cpp
@@ -0,0 +1,29 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-12-17 19:34:48
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        // move all the nonzero elements advance
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for (;j < nums.size(); j++) {
+            nums[j] = 0;
+        }
+    }
+};
+
+int main(){
+    return 0;
+}
