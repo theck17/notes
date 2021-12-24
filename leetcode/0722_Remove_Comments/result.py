3a3945150822a020bfc322cb0174dd3c54644201
theck17 <theck17@163.com>
2021-12-24 18:53:09 +0800

    722

---
+++ leetcode/0722_Remove_Comments/result.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-12-24 18:49:45
+# Description:
+class Solution:
+    def removeComments(self, source: List[str]) -> List[str]:
+        s = '\n'.join(source)
+        regex_single = r"\/\/.*"
+        regex_block_closed = r"(?s:\/\*.*?\*\/)"
+        regex_block_not_closed = r"\/\*.*"
+        regex = '|'.join(
+            [regex_single, regex_block_closed,
+             regex_block_not_closed]) # order matters
+        s = re.sub(regex, '', s)
+        return list(filter(None, s.split('\n')))
+
+
+if __name__ == "__main__":
+    pass
