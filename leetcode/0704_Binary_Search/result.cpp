a6b2daf5f01e0c7184dc405762f7e4cf302f691f
theck17 <theck17@163.com>
2021-12-25 18:52:23 +0800

    704

---
+++ leetcode/0704_Binary_Search/result.cpp
@@ -0,0 +1,28 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-12-25 18:51:30
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size()-1;
+        int low = 0, high = n;
+        while( low <= high){
+            int mid = low + (high-low)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) high = mid -1;
+            else low = mid + 1;
+        }
+        return -1;
+    }
+};
+
+int main(){
+    return 0;
+}
