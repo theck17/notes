59b392e0b179ffc665b44cde09afd767164d0f70
theck17 <theck17@163.com>
2021-12-25 18:51:26 +0800

    704

---
+++ leetcode/0704_Binary_Search/result.c
@@ -0,0 +1,32 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.c
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-12-25 18:50:51
+ *   Description   ：
+ */
+int search(int* nums, int numsSize, int target)
+{
+    int left = 0, right = numsSize - 1;
+    
+    while (left <= right)
+    {
+        int middle = left + (right - left) / 2;
+        
+        if (nums[middle] == target)
+            return middle;
+        
+        else if (nums[middle] < target)
+            left = middle + 1;
+        
+        else
+            right = middle - 1;
+    }
+    return -1;
+}
+
+int main(){
+    return 0;
+}
