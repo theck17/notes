bd3412f24963335a3eb50bedad69b8758b9d359f
theck17 <theck17@163.com>
2021-12-29 18:38:49 +0800

    150

---
+++ leetcode/0150_Evaluate_Reverse_Polish_Notation/result.cpp
@@ -0,0 +1,38 @@
+/**
+ *   Copyright (C) 2021 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2021-12-29 18:38:11
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        unordered_map<string, function<int (int, int) > > map = {
+            { "+" , [] (int a, int b) { return a + b; } },
+            { "-" , [] (int a, int b) { return a - b; } },
+            { "*" , [] (int a, int b) { return a * b; } },
+            { "/" , [] (int a, int b) { return a / b; } }
+        };
+        std::stack<int> stack;
+        for (string& s : tokens) {
+            if (!map.count(s)) {
+                stack.push(stoi(s));
+            } else {
+                int op1 = stack.top();
+                stack.pop();
+                int op2 = stack.top();
+                stack.pop();
+                stack.push(map[s](op2, op1));
+            }
+        }
+        return stack.top();
+    }
+};
+
+int main(){
+    return 0;
+}
