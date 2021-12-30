f725c52c2db1156bc4cfa3dd05fc5bc8a6aef2cd
theck17 <theck17@163.com>
2021-12-30 21:26:30 +0800

    520

---
+++ leetcode/0520_Detect_Capital/result.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2021-12-30 21:25:58
+# Description:
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        Num_cap, n = 0, len(word)
+        for letter in word:
+            Num_cap += letter.isupper()
+        if Num_cap == 0 or Num_cap == n or Num_cap == 1 and word[0].isupper():
+            return True
+        return False
+
+
+if __name__ == "__main__":
+    pass
