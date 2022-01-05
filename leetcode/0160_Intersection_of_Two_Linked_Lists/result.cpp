297a180be657402b7556021be44b1ff951df919a
theck17 <theck17@163.com>
2022-01-05 19:01:11 +0800

    160

---
+++ leetcode/0160_Intersection_of_Two_Linked_Lists/result.cpp
@@ -0,0 +1,33 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-01-05 19:00:14
+ *   Description   ：
+ */
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *cur1 = headA, *cur2 = headB;
+        while(cur1 != cur2){
+            cur1 = cur1?cur1->next:headB;
+            cur2 = cur2?cur2->next:headA;
+        }
+        return cur1;
+    }
+};
+
+int main(){
+    return 0;
+}
