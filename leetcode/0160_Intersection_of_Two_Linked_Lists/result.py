4d0e060362a1d838b8b96be44be90b908e144232
theck17 <theck17@163.com>
2022-01-05 19:03:36 +0800

    160

---
+++ leetcode/0160_Intersection_of_Two_Linked_Lists/result.py
@@ -0,0 +1,32 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2022-01-05 19:02:18
+# Description:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode,
+                            headB: ListNode) -> ListNode:
+        if headA is None or headB is None:
+            return None
+
+        pa = headA # 2 pointers
+        pb = headB
+
+        while pa is not pb:
+            # if either pointer hits the end, switch head and continue the second traversal,
+            # if not hit the end, just move on to next
+            pa = headB if pa is None else pa.next
+            pb = headA if pb is None else pb.next
+
+        return pa # only 2 ways to get out of the loop, they meet or the both hit the end=None
+
+
+if __name__ == "__main__":
+    pass
