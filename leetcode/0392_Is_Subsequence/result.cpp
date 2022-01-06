74e9a1bfdfa6a778a4b90628c38647788e83b309
theck17 <theck17@163.com>
2022-01-06 21:21:13 +0800

    392

---
+++ leetcode/0392_Is_Subsequence/result.cpp
@@ -0,0 +1,33 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-01-06 21:20:15
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    bool isSubSeq(string str1, string str2, int m, int n) 
+{ 
+    // Base Cases 
+    if (m == 0) return true; 
+    if (n == 0) return false; 
+    // If last characters of two strings are matching 
+    if (str1[m-1] == str2[n-1]) 
+        return isSubSeq(str1, str2, m-1, n-1); 
+    // If last characters are not matching 
+    return isSubSeq(str1, str2, m, n-1); 
+} 
+    bool isSubsequence(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        return isSubSeq(s,t,m,n);
+    }
+};
+
+int main(){
+    return 0;
+}
