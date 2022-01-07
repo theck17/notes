0b56d754403128e73d3c3b2376cf6eeeeb89bd45
theck17 <theck17@163.com>
2022-01-07 19:01:16 +0800

    103

---
+++ leetcode/0103_Binary_Tree_Zigzag_Level_Order_Traversal/result.cpp
@@ -0,0 +1,62 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-01-07 19:00:15
+ *   Description   ：
+ */
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int> > zigzagLevelOrder(TreeNode* root) {
+        if (root == NULL) {
+            return vector<vector<int> > ();
+        }
+        vector<vector<int> > result;
+
+        queue<TreeNode*> nodesQueue;
+        nodesQueue.push(root);
+        bool leftToRight = true;
+
+        while ( !nodesQueue.empty()) {
+            int size = nodesQueue.size();
+            vector<int> row(size);
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = nodesQueue.front();
+                nodesQueue.pop();
+
+                // find position to fill node's value
+                int index = (leftToRight) ? i : size - 1 - i);
+
+                row[index] = node->val;
+                if (node->left) {
+                    nodesQueue.push(node->left);
+                }
+                if (node->right) {
+                    nodesQueue.push(node->right);
+                }
+            }
+            // after this level
+            leftToRight = !leftToRight;
+            result.push_back(row);
+        }
+        return result;
+    }
+};(
+
+int main(){
+    return 0;
+}
