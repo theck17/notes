969fc3093dd645b8e8c29bde1a0c2bd029b46a35
theck17 <theck17@163.com>
2022-01-08 09:06:38 +0800

    278

---
+++ leetcode/0278_First_Bad_Version/result.py
@@ -0,0 +1,23 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2022-01-08 09:05:41
+# Description:
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+
+if __name__ == "__main__":
+    pass
