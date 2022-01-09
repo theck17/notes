73a989eddfb62a1b147ad8ff6408b39cc7631a20
theck17 <theck17@163.com>
2022-01-09 09:14:51 +0800

    171

---
+++ leetcode/0171_Excel_Sheet_Column_Number/result.cpp
@@ -0,0 +1,24 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-01-09 09:14:26
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int col = 0;
+        for(int i = s.length(); i > 0; i--) {
+            col += (s[i - 1] - 'A' + 1) * pow(26, (s.length() - i));
+        }
+        return col;
+    }
+};
+
+int main(){
+    return 0;
+}
