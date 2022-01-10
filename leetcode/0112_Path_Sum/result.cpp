f85c1650c2f49264d1287ad66e4413b99fff2a89
theck17 <theck17@163.com>
2022-01-10 18:37:15 +0800

    112

---
+++ leetcode/0112_Path_Sum/result.cpp
@@ -0,0 +1,34 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-01-10 18:36:12
+ *   Description   ：
+ */
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(!root)return false;                                         //Terminating Condition
+        sum=sum-root->val;                                             //Body
+        if(sum==0&&!root->left&&!root->right)return true;              //body
+        return hasPathSum(root->left,sum)||hasPathSum(root->right,sum);//Propagation
+    }
+};
+
+int main(){
+    return 0;
+}
