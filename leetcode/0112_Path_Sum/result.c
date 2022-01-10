921f5ee590de211c1984e453e29c9a97591c9684
theck17 <theck17@163.com>
2022-01-10 18:38:02 +0800

    112

---
+++ leetcode/0112_Path_Sum/result.c
@@ -0,0 +1,37 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.c
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-01-10 18:37:19
+ *   Description   ：
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+bool count(struct TreeNode *root,int sum)
+{
+    if(root==NULL) return false;
+    sum-=root->val;
+    if(sum==0&&root->left==NULL&&root->right==NULL)
+        return true;
+    return (count(root->left,sum)||count(root->right,sum));
+}
+
+bool hasPathSum(struct TreeNode* root, int sum){
+    if (root==NULL) 
+        return false;
+    return count(root,sum);
+}
+
+int main(){
+    return 0;
+}
