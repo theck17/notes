aeb5d7ae8250f431f4f69107f8be869c331b2dac
theck17 <theck17@163.com>
2022-01-11 23:38:09 +0800

    367

---
+++ leetcode/0367_Valid_Perfect_Square/result.cpp
@@ -0,0 +1,26 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-01-11 23:34:49
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int a=1;
+        
+        while(num>0){
+            num-=a;
+            a+=2;
+        }
+        return(num==0);
+    }
+};
+
+int main(){
+    return 0;
+}
