535b6ced30a85b01e2f05304afe305f191b8e9d1
theck17 <theck17@163.com>
2022-01-19 18:47:23 +0800

    345

---
+++ leetcode/0345_Reverse_Vowels_of_a_String/result.cpp
@@ -0,0 +1,28 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-01-19 18:46:50
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            i = s.find_first_of("aeiouAEIOU", i);
+            j = s.find_last_of("aeiouAEIOU", j);
+            if (i < j) {
+                swap(s[i++], s[j--]);
+            }
+        }
+        return s;
+    }
+};
+
+int main(){
+    return 0;
+}
