9d08754856031d832277323a8e6e0a8dc7d80ec6
theck17 <theck17@163.com>
2022-01-21 10:47:21 +0800

    0009

---
+++ leetcode/0009_Palindrome_Number/result.cpp
@@ -0,0 +1,27 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-01-21 10:46:49
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0|| (x!=0 &&x%10==0)) return false;
+        int sum=0;
+        while(x>sum)
+        {
+            sum = sum*10+x%10;
+            x = x/10;
+        }
+        return (x==sum)||(x==sum/10);
+    }
+};
+
+int main(){
+    return 0;
+}
