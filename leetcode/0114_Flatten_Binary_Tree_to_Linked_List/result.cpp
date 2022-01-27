b15ed33298510e3a1a067380501273983167f3d6
theck17 <theck17@163.com>
2022-01-27 18:38:10 +0800

    114

---
+++ leetcode/0114_Flatten_Binary_Tree_to_Linked_List/result.cpp
@@ -0,0 +1,50 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-01-27 18:37:13
+ *   Description   ：
+ */
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if( root )
+        {
+            /*Move the left node to the right node*/
+            TreeNode* temp = root->right;
+            root->right = root->left;
+            root->left = nullptr;
+            TreeNode* node = root;
+            
+            /*Move to the end of the prev left node which is the new right node*/
+            while( node->right )
+            {
+                node = node->right;
+            }
+            
+            /*Append the right node to its end*/
+            node->right = temp;
+            flatten( root->right ); 
+        } 
+        return;
+               
+    }
+};
+
+int main(){
+    return 0;
+}
