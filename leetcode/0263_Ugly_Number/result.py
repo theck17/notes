8afe18563aab1b0ac3ee0e84e45c056b48dfc662
theck17 <theck17@163.com>
2022-01-28 22:13:06 +0800

    263

---
+++ leetcode/0263_Ugly_Number/result.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2022-01-28 22:12:00
+# Description:
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n == 0: return False
+        while n % 5 == 0:
+            n /= 5
+        while n % 3 == 0:
+            n /= 3
+        while n % 2 == 0:
+            n /= 2
+        return n == 1
+
+
+if __name__ == "__main__":
+    pass
