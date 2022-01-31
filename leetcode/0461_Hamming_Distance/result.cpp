c2e17a1b3791c2002a3b329f106309e5fd9e1209
theck17 <theck17@163.com>
2022-01-31 10:52:39 +0800

    461

---
+++ leetcode/0461_Hamming_Distance/result.cpp
@@ -0,0 +1,25 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-01-31 10:51:22
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int dist = 0, n = x ^ y;
+        while (n) {
+            ++dist;
+            n &= n - 1;
+        }
+        return dist;
+    }
+};
+
+int main(){
+    return 0;
+}
