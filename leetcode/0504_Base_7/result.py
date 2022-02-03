ef2149a80800f99e9bb868008c1424e49f7232ca
theck17 <theck17@163.com>
2022-02-03 20:13:41 +0800

    504

---
+++ leetcode/0504_Base_7/result.py
@@ -0,0 +1,23 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2022-02-03 20:13:06
+# Description:
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if not num:
+            return '0'
+
+        sign = num < 0
+        num = abs(num)
+
+        stack = []
+        while num:
+            stack.append(str(num % 7))
+            num = num // 7
+
+        return '-' * sign + ''.join(stack[::-1])
+
+
+if __name__ == "__main__":
+    pass
