e56b71f29f27605fe880b5ec6581dd4b75cc0c38
theck17 <theck17@163.com>
2022-02-10 10:12:29 +0800

    221

---
+++ leetcode/0221_Maximal_Square/result.cpp
@@ -0,0 +1,35 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-02-10 10:07:20
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty()) {
+            return 0;
+        }
+        int m = matrix.size(), n = matrix[0].size(), sz = 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!i || !j || matrix[i][j] == '0') {
+                    dp[i][j] = matrix[i][j] - '0';
+                } else {
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+                sz = max(dp[i][j], sz);
+            }
+        }
+        return sz * sz;
+    }
+};
+
+int main(){
+    return 0;
+}
