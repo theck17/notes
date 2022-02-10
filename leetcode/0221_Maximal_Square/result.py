f3a2672044541c686ddcbc67b548763f6b22fc6e
theck17 <theck17@163.com>
2022-02-10 10:31:13 +0800

    221

---
+++ leetcode/0221_Maximal_Square/result.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2022-02-10 10:27:54
+# Description: 
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if matrix is None or len(matrix) < 1:
+            return 0
+        
+        rows = len(matrix)
+        cols = len(matrix[0])
+        
+        dp = [[0]*(cols+1) for _ in range(rows+1)]
+        max_side = 0
+        
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == '1':
+                    dp[r+1][c+1] = min(dp[r][c], dp[r+1][c], dp[r][c+1]) + 1 # Be careful of the indexing since dp grid has additional row and column
+                    max_side = max(max_side, dp[r+1][c+1])
+                
+        return max_side * max_side
+
+if __name__ == "__main__":
+   pass
+
