762ac7aa4686a9cc15f8a36be964b832cefaa7b4
theck17 <theck17@163.com>
2022-02-12 12:13:36 +0800

    563

---
+++ leetcode/0563_Binary_Tree_Tilt/result.cpp
@@ -0,0 +1,40 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-02-12 12:12:55
+ *   Description   ：
+ */
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int tiltSum = 0;
+    int findTilt(TreeNode* root) {
+        DFS(root);
+        return tiltSum;
+    }
+    int DFS(TreeNode* root) {
+        if(!root) return 0;
+        int leftSum = DFS(root -> left);                // sum of left subtree
+        int rightSum = DFS(root -> right);              // sum of right subtree
+        tiltSum += abs(leftSum - rightSum);             // add tilt of current node to overall tiltSum
+        return leftSum + rightSum + root -> val;        // returns sum of subtree starting at 'root'
+    }
+};
+
+int main(){
+    return 0;
+}
