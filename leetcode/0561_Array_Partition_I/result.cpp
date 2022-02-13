108d093cb25692be601893a7a843fd26e8ae5fc9
theck17 <theck17@163.com>
2022-02-13 10:59:52 +0800

    561

---
+++ leetcode/0561_Array_Partition_I/result.cpp
@@ -0,0 +1,37 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-02-13 10:59:03
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        vector<int> hashtable(20001,0);
+        for(size_t i=0;i<nums.size();i++)
+        {
+            hashtable[nums[i]+10000]++;
+        }
+        int ret=0;
+        int flag=0;
+        for(size_t i=0;i<20001;){
+            if((hashtable[i]>0)&&(flag==0)){
+                ret=ret+i-10000;
+                flag=1;
+                hashtable[i]--;
+            }else if((hashtable[i]>0)&&(flag==1)){
+                hashtable[i]--;
+                flag=0;
+            }else i++;
+        }
+        return ret;
+    }
+};
+
+int main(){
+    return 0;
+}
