d0ac9c09ba5bc6e7d5aec4cdb058c3169684a198
theck17 <theck17@163.com>
2022-02-16 08:04:32 +0800

    812

---
+++ leetcode/0812_Largest_Triangle_Area/result.cpp
@@ -0,0 +1,25 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-02-16 08:03:34
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        double res = 0;
+        for (auto &i : points)
+            for (auto &j : points)
+                for (auto &k : points)
+            res = max(res, 0.5 * abs(i[0] * j[1] + j[0] * k[1] + k[0] * i[1]- j[0] * i[1] - k[0] * j[1] - i[0] * k[1]));
+        return res;
+    }
+};
+
+int main(){
+    return 0;
+}
