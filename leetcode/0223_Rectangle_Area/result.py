ce556526e9673fd21f1b0203dabd13e199f801d4
theck17 <theck17@163.com>
2022-02-17 15:16:27 +0800

    223

---
+++ leetcode/0223_Rectangle_Area/result.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2022-02-17 15:14:39
+# Description:
+class Solution:
+    def computeArea(self, A: int, B: int, C: int, D: int, E: int, F: int,
+                    G: int, H: int) -> int:
+        overlap = max(min(C, G) - max(A, E), 0) * max(min(D, H) - max(B, F), 0)
+        return (A - C) * (B - D) + (E - G) * (F - H) - overlap
+
+
+if __name__ == "__main__":
+    pass
