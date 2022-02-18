33a9bb94a7781f6fe41e8d9276aea8a15f201545
theck17 <theck17@163.com>
2022-02-18 10:40:04 +0800

    319

---
+++ leetcode/0319_Bulb_Switcher/result.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2022-02-18 10:38:54
+# Description:
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(n ** (1 / 2))
+
+
+if __name__ == "__main__":
+    pass
