f6404ae2517ff476ce58d354f0b68a0a075fbe89
theck17 <theck17@163.com>
2022-02-18 10:38:51 +0800

    319

---
+++ leetcode/0319_Bulb_Switcher/result.c
@@ -0,0 +1,16 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.c
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-02-18 10:37:19
+ *   Description   ：
+ */
+int bulbSwitch(int n){
+    return sqrt(n);
+}
+
+int main(){
+    return 0;
+}
