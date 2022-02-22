5a9435835b068a8faf8e7a1e00559b352156fa75
theck17 <theck17@163.com>
2022-02-22 09:59:21 +0800

    492

---
+++ leetcode/0492_Construct_the_Rectangle/result.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2022-02-22 09:57:44
+# Description:
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        for l in range(int(area ** 0.5), 0, -1):
+            if area % l == 0:
+                return [area // l, l]
+
+
+if __name__ == "__main__":
+    pass
