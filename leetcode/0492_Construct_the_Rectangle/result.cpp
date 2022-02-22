be221c0abc55f4c1b6e18b8408fc6bf0bac830eb
theck17 <theck17@163.com>
2022-02-22 09:56:31 +0800

    492

---
+++ leetcode/0492_Construct_the_Rectangle/result.cpp
@@ -0,0 +1,25 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-02-22 09:53:38
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        int mid = sqrt(area);
+        while(mid > 0){
+            if(area % mid == 0) return {area/mid, mid};
+            mid--;
+        }
+        return {0, 0};
+    }
+};
+
+int main(){
+    return 0;
+}
