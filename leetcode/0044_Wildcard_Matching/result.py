d1a040eb7518fadb413e87655d2ab4ec96d6df37
theck17 <theck17@163.com>
2022-02-26 00:41:25 +0800

    44

---
+++ leetcode/0044_Wildcard_Matching/result.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2022-02-26 00:39:58
+# Description:
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False for _ in range(len(p) + 1)] for i in range(len(s) + 1)]
+        dp[0][0] = True
+        for j in range(1, len(p) + 1):
+            if p[j - 1] != '*':
+                break
+            dp[0][j] = True
+
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(p) + 1):
+                if p[j - 1] in {s[i - 1], '?'}:
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
+        return dp[-1][-1]
+
+
+if __name__ == "__main__":
+    pass
