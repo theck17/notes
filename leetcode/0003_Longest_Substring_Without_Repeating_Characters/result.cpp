e16b48f3ef1d2dca5815d3584a36499daad0b9d3
theck17 <theck17@163.com>
2022-02-25 09:06:22 +0800

    3

---
+++ leetcode/0003_Longest_Substring_Without_Repeating_Characters/result.cpp
@@ -0,0 +1,28 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-02-25 09:05:59
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
+};
+
+int main(){
+    return 0;
+}
