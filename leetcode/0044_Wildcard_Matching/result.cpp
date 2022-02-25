3b80a0ef3a37bbc00875f1878ab1ab5c103dbb5d
theck17 <theck17@163.com>
2022-02-26 00:39:07 +0800

    44

---
+++ leetcode/0044_Wildcard_Matching/result.cpp
@@ -0,0 +1,35 @@
+/**
+ *   Copyright (C) 2022 All rights reserved.
+ *
+ *   FileName      ：result.cpp
+ *   Author        ：C.K
+ *   Email         ：theck17@163.com
+ *   DateTime      ：2022-02-26 00:38:23
+ *   Description   ：
+ */
+using namespace std;
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length(), n = p.length();
+        if (n > 30000) return false; // the trick
+        vector<bool> cur(m + 1, false); 
+        cur[0] = true;
+        for (int j = 1; j <= n; j++) {
+            bool pre = cur[0]; // use the value before update
+            cur[0] = cur[0] && p[j - 1] == '*'; 
+            for (int i = 1; i <= m; i++) {
+                bool temp = cur[i]; // record the value before update
+                if (p[j - 1] != '*')
+                    cur[i] = pre && (s[i - 1] == p[j - 1] || p[j - 1] == '?');
+                else cur[i] = cur[i - 1] || cur[i];
+                pre = temp;
+            }
+        }
+        return cur[m]; 
+    }
+};
+
+int main(){
+    return 0;
+}
