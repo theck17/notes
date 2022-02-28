8be92ed779195597a5d5fe6732be2025131cb1b3
theck17 <theck17@163.com>
2022-02-28 11:18:34 +0800

    700

---
+++ leetcode/0700_Search_in_a_Binary_Search_Tree/result.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python3
+# Author: C.K
+# Email: theck17@163.com
+# DateTime:2022-02-28 11:17:54
+# Description:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return
+        if root.val == val:
+            return root
+        if root.val < val:
+            return self.searchBST(root.right, val)
+        else:
+            return self.searchBST(root.left, val)
+
+
+if __name__ == "__main__":
+    pass
