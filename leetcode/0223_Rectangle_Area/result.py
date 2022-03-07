77da69b42f22552abc0e97645be5f2af2106a67b
theck17 <theck17@163.com>
2022-03-07 08:27:18 +0800

    223

--- leetcode/0223_Rectangle_Area/result.py
+++ leetcode/0223_Rectangle_Area/result.py
@@ -8,7 +8,3 @@ class Solution:
                     G: int, H: int) -> int:
         overlap = max(min(C, G) - max(A, E), 0) * max(min(D, H) - max(B, F), 0)
         return (A - C) * (B - D) + (E - G) * (F - H) - overlap
-
-
-if __name__ == "__main__":
-    pass
