fd72d76073f47661bff7a3ce9388b1bf4cda1eca
theck17 <theck17@163.com>
2022-04-06 00:12:14 +0800

    169

--- leetcode/0169_Majority_Element/result.py
+++ leetcode/0169_Majority_Element/result.py
@@ -3,15 +3,9 @@
 # Email: theck17@163.com
 # DateTime:2021-06-05 11:23:41
 # Description:
-import os
-import sys
 
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         numsa.sort()
         return nums[len(nums) // 2]
-
-
-if __name__ == "__main__":
-    pass
