0f03a5f521089b234479c11e27828887e922981f
theck17 <theck17@163.com>
2022-04-11 10:27:25 +0800

    31

--- leetcode/0031_Next_Permutation/result.py
+++ leetcode/0031_Next_Permutation/result.py
@@ -2,7 +2,8 @@
 # Author: C.K
 # Email: theck17@163.com
 # DateTime:2021-03-20 18:14:01
-# Description: 
+# Description:
+
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
@@ -10,29 +11,21 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         for i in range(len(nums) - 1, 0, -1):
-            if nums[i] > nums[i-1]:
+            if nums[i] > nums[i - 1]:
                 # find the mininum item that's greater than nums[i-1]
                 min_v = inf
                 k = 0
                 for j in range(i, len(nums)):
-                    if nums[j] > nums[i-1] and nums[j] < min_v:
+                    if nums[j] > nums[i - 1] and nums[j] < min_v:
                         k = j
                         min_v = nums[j]
-                
+
                 # exchange
-                nums[i-1], nums[k] = nums[k], nums[i-1]
-                
+                nums[i - 1], nums[k] = nums[k], nums[i - 1]
+
                 # sort
                 nums[i:] = sorted(nums[i:])
                 return
-        
+
         # otherwise
         nums.sort()
-        
-
-
-
-
-if __name__ == "__main__":
-   pass
-
